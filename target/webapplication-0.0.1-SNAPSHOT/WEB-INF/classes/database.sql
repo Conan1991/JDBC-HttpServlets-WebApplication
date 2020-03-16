1c792059221f87a4d87f4a95323adb1b072e9f11
Vitaliy Doronin <vitaliy.doronin@accenture.com>
2020-03-16 17:10:44 +0300

    remove unusable folders

--- target/webapplication-0.0.1-SNAPSHOT/WEB-INF/classes/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE USER (
-  ID UUID DEFAULT random_uuid() PRIMARY KEY,
-  LOGIN VARCHAR(100) NOT NULL,
-  PASSWORD VARCHAR(20) NOT NULL,
-  FIO VARCHAR,
-  ROLE VARCHAR NOT NULL DEFAULT 'Employee' CONSTRAINT def_c CHECK (ROLE IN ('Employee', 'Client'))
-);
-
-insert into user values ( default, 'Maria', '123', 'Petrova Maria Aleksandrovna', default );
